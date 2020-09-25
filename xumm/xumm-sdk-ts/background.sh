232944652710a9f974c446dba62fc8461c142890
Wietse Wind <mail@wietse.com>
2020-09-25 15:16:30 +0200

    Try no VS code welcome page

--- xumm/xumm-sdk-ts/background.sh
+++ xumm/xumm-sdk-ts/background.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo '{"files.exclude": {".*": true}, "workbench.startupEditor": "none"}' > ~/.vscode/settings.json
+
 echo "This is a background script with a long running process"
 sleep 2
 echo "done" >> /opt/.backgroundfinished
