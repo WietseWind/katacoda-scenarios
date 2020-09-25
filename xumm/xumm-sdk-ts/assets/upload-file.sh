5010de3f1cfcf76217a7aceb96c568db35542fa5
Wietse Wind <mail@wietse.com>
2020-09-25 15:03:17 +0200

    More testing

--- xumm/xumm-sdk-ts/assets/upload-file.sh
+++ xumm/xumm-sdk-ts/assets/upload-file.sh
@@ -1,8 +1,3 @@
-"details": {
-    "assets": {
-        "host01": [
-            {"file": "wait.sh", "target": "/usr/local/bin/", "chmod": "+x"},
-            {"file": "deploy.sh", "target": "/usr/local/bin/", "chmod": "+x"}
-        ]
-    }
-},
+#!/bin/bash
+
+echo "Hi, this is an uploaded file."
